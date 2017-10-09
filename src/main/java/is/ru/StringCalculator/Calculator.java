77c5b69429671dfb08cc2c7acf9ec99b13623596
Gudberg Sumarlidason <gudberg_sumarlidason@Gudberg.lan>
2017-10-09 20:11:13 +0000

    Test to see whether it works adding two numbers

--- src/main/java/is/ru/StringCalculator/Calculator.java
+++ src/main/java/is/ru/StringCalculator/Calculator.java
@@ -10,6 +10,11 @@ public class Calculator {
 			return 0;
 		}
 		else {
+			if(text.contains(","))
+			{
+				String numbers [] = text.split(",");
+				return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+			}
 			return 1;
 		}
 	}
